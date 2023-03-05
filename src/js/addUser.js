3cc0a7dd7a56d2f2eab7b8189f05aca5cd87953d
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-03-05 13:40:02 +0900

    refactor: #29 refactoring for all features code

---
+++ src/js/addUser.js
@@ -0,0 +1,117 @@
+export default function addUser(map) {
+  const $btnOpenSearch = document.querySelector('.nav-item.item-user');
+  const $btnOpenResult = document.querySelector('.nav-item.item-map-location-result');
+  const $sectionSearch = document.querySelector('.section-search');
+  const $searchList = document.querySelector('.search-user-list');
+  const $inputName = $sectionSearch.querySelector('.input-name');
+  const $inputAddr = $sectionSearch.querySelector('.input-addr');
+
+  const markers = [];
+
+  const openSearchPopup = () => {
+    $sectionSearch.classList.toggle('is-active');
+  };
+
+  const addParticipantsItem = ({ name, addr }) => {
+    const li = document.createElement('li');
+    const participant = document.createElement('strong');
+    const address = document.createElement('p');
+
+    participant.textContent = name;
+    address.textContent = addr;
+
+    li.appendChild(participant);
+    li.appendChild(address);
+    $searchList.appendChild(li);
+  };
+
+  const searchAddr = () => {
+    if ($inputName.value.length === 0) {
+      alert('참석자의 이름을 먼저 입력해주세요.');
+      $inputName.focus();
+      return;
+    }
+
+    const name = $inputName.value;
+
+    new daum.Postcode({
+      oncomplete: function (data) {
+        // 포스트 코드 입력 후 주소를 지오코더로 변환한다
+        const geocoder = new kakao.maps.services.Geocoder();
+        const callback = function (result, status) {
+          if (status === kakao.maps.services.Status.OK) {
+            // 마커 생성
+            const marker = new kakao.maps.Marker({
+              map, // 마커를 표시할 지도
+              position: new kakao.maps.LatLng(result[0].y, result[0].x), // 마커의 위치
+            });
+
+            const infoWindow = new kakao.maps.InfoWindow({
+              content: `<div class='info-window' data-lng=${result[0].x} data-lat=${result[0].y}>${name}</div>`,
+            });
+            infoWindow.open(map, marker);
+
+            // 지오코더로 변환 완료 후 (빈)배열에 푸쉬
+            markers.push({
+              position: new kakao.maps.LatLng(result[0].y, result[0].x),
+            });
+
+            // 이미지 지도의 중심좌표
+            const bounds = new kakao.maps.LatLngBounds();
+            markers.forEach(v => {
+              bounds.extend(v.position);
+            });
+            map.setBounds(bounds);
+          }
+        };
+        geocoder.addressSearch(data.address, callback);
+
+        addParticipantsItem({ name, addr: data.address });
+        $inputName.value = '';
+        $inputAddr.value = '';
+
+        if ($searchList.childElementCount > 1) {
+          $btnOpenResult.classList.add('is-active');
+        }
+      },
+    }).open();
+  };
+
+  const test = () => {
+    const markers = [
+      {
+        position: new kakao.maps.LatLng(37.5697299794152, 126.988067892251),
+      },
+      {
+        position: new kakao.maps.LatLng(37.5385087413674, 127.10002339294),
+      },
+    ];
+
+    for (var i = 0; i < markers.length; i++) {
+      var marker = new kakao.maps.Marker({
+        map,
+        position: markers[i].position,
+      });
+
+      const infoWindow = new kakao.maps.InfoWindow({
+        content: `<div class='info-window' data-lng=${markers[i].position.La} data-lat=${markers[i].position.Ma}>${'테스트'}</div>`,
+      });
+      infoWindow.open(map, marker);
+
+      addParticipantsItem({ name: '테스트', addr: '종로2가' });
+    }
+
+    if ($searchList.childElementCount > 1) {
+      $btnOpenResult.classList.add('is-active');
+    }
+
+    let bounds = new kakao.maps.LatLngBounds();
+    markers.forEach(v => {
+      bounds.extend(v.position);
+    });
+    map.setBounds(bounds);
+  };
+
+  $btnOpenSearch.addEventListener('click', openSearchPopup);
+  $inputAddr.addEventListener('click', searchAddr);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,41 @@
-const webpack = require("webpack");
-const path = require("path");
-const dotenv = require("dotenv");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const dotenv = require('dotenv');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 dotenv.config();
 
-console.log(process.env.KAKAO_KEY);
-
 module.exports = {
-  entry: "./src/js/index.js",
+  entry: './src/js/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  mode: "development",
+  mode: 'development',
   plugins: [
-    // dotenv 사용을 위한 설정
     new webpack.DefinePlugin({
       KAKAO_KEY: JSON.stringify(process.env.KAKAO_KEY),
       PUBLIC_KEY: JSON.stringify(process.env.PUBLIC_KEY),
     }),
     new HtmlWebpackPlugin({
-      template: "./index.html", // index.html을 기본 템플릿으로 반영할 수 있도록 설정
+      template: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "common.css",
+      filename: 'common.css',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
   devServer: {
-    // 개발 서버가 dist 폴더를 제공할 수 있도록 설정
     static: {
-      directory: path.resolve(__dirname, "dist"),
+      directory: path.resolve(__dirname, 'dist'),
     },
     port: 5507,
   },
